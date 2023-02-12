5f433ca5925d44caa4da7abfd523548db94b8515
Juan Hamilton <hedwards507@gmail.com>
2023-02-12 05:35:27 -0500

    Add initial setup, structure & connect to firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'sweeties672-4f600',
+    appId: '1:774831250273:web:4ce862d57850a626fa0320',
+    storageBucket: 'sweeties672-4f600.appspot.com',
+    apiKey: 'AIzaSyAkV3WOeVsvHg3pDv1DH7eydbAmsA3jb7E',
+    authDomain: 'sweeties672-4f600.firebaseapp.com',
+    messagingSenderId: '774831250273',
+    measurementId: 'G-D8VTFGW82C',
+  },};
