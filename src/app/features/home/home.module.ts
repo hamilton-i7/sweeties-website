4a2db199bf82d8613bcfdfd8dd848af1269330e4
Juan Hamilton <hedwards507@gmail.com>
2023-02-13 04:56:36 -0500

    Create BenefitsComponent

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { ShareModule } from '../../share/share.module';
+import { BenefitsComponent } from './components/benefits/benefits.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent],
+  declarations: [HomeComponent, HeroComponent, BenefitsComponent],
   imports: [CommonModule, ShareModule],
   exports: [HomeComponent],
 })
