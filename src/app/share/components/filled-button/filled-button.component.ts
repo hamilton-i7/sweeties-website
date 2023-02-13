84a8b45249da6a3116c23e1f0296154cc941afe7
Juan Hamilton <hedwards507@gmail.com>
2023-02-13 04:53:48 -0500

    Add styles to Home/HeroComponent

--- src/app/share/components/filled-button/filled-button.component.ts
+++ src/app/share/components/filled-button/filled-button.component.ts
@@ -1,10 +1,12 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-filled-button',
   templateUrl: './filled-button.component.html',
 })
 export class FilledButtonComponent {
+  @Input() href?: string;
+  @Input() newTab = false;
   @Output() btnClick = new EventEmitter();
 
   onClick() {
