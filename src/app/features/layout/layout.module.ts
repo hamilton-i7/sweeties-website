ff394c5a347cee11f4dc15b2a180174f71a07518
Juan Hamilton <hedwards507@gmail.com>
2023-02-13 23:46:56 -0500

    Create footer and add basic styling

--- src/app/features/layout/layout.module.ts
+++ src/app/features/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LayoutModule as MatLayoutModule } from '@angular/cdk/layout';
 import { ShareModule } from '../../share/share.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, MatLayoutModule, ShareModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
