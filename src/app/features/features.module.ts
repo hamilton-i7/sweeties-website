724975963bdd3bedd1088738573eb8445a2a813a
Juan Hamilton <hedwards507@gmail.com>
2023-02-15 02:36:39 -0500

    Create Product feature

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
 import { HomeModule } from './home/home.module';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductComponent
+  ],
   imports: [CommonModule, LayoutModule, HomeModule],
   exports: [LayoutModule, HomeModule],
 })
