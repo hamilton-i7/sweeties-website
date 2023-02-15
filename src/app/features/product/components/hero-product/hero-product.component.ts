94697f0718ce075fd8978e052d54e7f0cfe6307a
Juan Hamilton <hedwards507@gmail.com>
2023-02-15 05:27:01 -0500

    Create Products page

---
+++ src/app/features/product/components/hero-product/hero-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-product',
+  templateUrl: './hero-product.component.html',
+  styleUrls: ['./hero-product.component.scss']
+})
+export class HeroProductComponent {
+
+}
--- src/app/features/product/product.module.ts
+++ src/app/features/product/product.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
+import { HeroProductComponent } from './components/hero-product/hero-product.component';
 
 @NgModule({
-  declarations: [ProductComponent],
+  declarations: [ProductComponent, HeroProductComponent],
   imports: [CommonModule],
   exports: [ProductComponent],
 })
