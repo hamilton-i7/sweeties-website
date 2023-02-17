5c69234f133c54725dfb173413aecadf6a1b87ca
Juan Hamilton <hedwards507@gmail.com>
2023-02-17 08:29:11 -0500

    Add content & styles to Buy page hero

--- src/app/features/buy/buy.module.ts
+++ src/app/features/buy/buy.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BuyComponent } from './buy.component';
 import { RouterModule } from '@angular/router';
+import { HeroBuyComponent } from './components/hero-buy/hero-buy.component';
+import { ShareModule } from '../../share/share.module';
 
 @NgModule({
-  declarations: [BuyComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [BuyComponent, HeroBuyComponent],
+  imports: [CommonModule, RouterModule, ShareModule],
   exports: [BuyComponent],
 })
 export class BuyModule {}
---
+++ src/app/features/buy/components/hero-buy/hero-buy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-buy',
+  templateUrl: './hero-buy.component.html',
+  styleUrls: ['./hero-buy.component.scss']
+})
+export class HeroBuyComponent {
+
+}
