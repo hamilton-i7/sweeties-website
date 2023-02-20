31ff885e5a6c2a22deeccc586c37a33867d4f356
Juan Hamilton <hedwards507@gmail.com>
2023-02-20 04:29:23 -0500

    Add error handling

--- src/app/features/product/services/product.service.ts
+++ src/app/features/product/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { collectionData, Firestore } from '@angular/fire/firestore';
-import { collection } from '@firebase/firestore';
-import { Observable } from 'rxjs';
+import { collection, FirestoreError } from '@firebase/firestore';
+import { catchError, Observable, of } from 'rxjs';
 import { PATH_PRODUCTS } from '../../../core/constants/product';
 import { productConverter } from '../../../core/converters/product';
 import { IProduct } from '../../../core/models/product';
@@ -16,6 +16,15 @@ export class ProductService {
     const ref = collection(this.firestore, PATH_PRODUCTS).withConverter(
       productConverter
     );
-    return collectionData(ref, { idField: 'id' });
+    return collectionData(ref, { idField: 'id' }).pipe(
+      catchError(this.handleError<IProduct[]>('getProducts', []))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: FirestoreError): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
